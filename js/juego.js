be9709e459a204f2ec43875096ebad0b5c73b483
UAIFede <ClaudioFederico.Riquelme@alumnos.uai.edu.ar>
2026-08-02 22:45:55 -0300

    Agrego juego.js donde se define datos del juego y se hace la mezcla aleatoria

---
+++ js/juego.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var PARES_POR_NIVEL = {
+    facil: 8,
+    medio: 10,
+    dificil: 18
+};
+
+var SIMBOLOS = [
+    { archivo: 'boca_jrs.svg', nombre: 'Boca Juniors' },
+    { archivo: 'river_plate.svg', nombre: 'River Plate' },
+    { archivo: 'independiente.svg', nombre: 'Independiente' },
+    { archivo: 'racing.svg', nombre: 'Racing Club' },
+    { archivo: 'san_lorenzo.svg', nombre: 'San Lorenzo' },
+    { archivo: 'huracan.svg', nombre: 'Huracán' },
+    { archivo: 'velez.svg', nombre: 'Vélez Sarsfield' },
+    { archivo: 'estudiantes.svg', nombre: 'Estudiantes de La Plata' },
+    { archivo: 'gimnasia.svg', nombre: 'Gimnasia y Esgrima La Plata' },
+    { archivo: 'rosario_central.svg', nombre: 'Rosario Central' },
+    { archivo: 'newells.svg', nombre: 'Newell\'s Old Boys' },
+    { archivo: 'talleres.svg', nombre: 'Talleres' },
+    { archivo: 'belgrano.svg', nombre: 'Belgrano' },
+    { archivo: 'lanus.svg', nombre: 'Lanús' },
+    { archivo: 'banfield.svg', nombre: 'Banfield' },
+    { archivo: 'tigre.svg', nombre: 'Tigre' },
+    { archivo: 'defensa.svg', nombre: 'Defensa y Justicia' },
+    { archivo: 'argentinos_jrs.svg', nombre: 'Argentinos Juniors' }
+];
+
+function mezclarArreglo(arreglo) {
+    var indice;
+    var aleatorio;
+    var temporal;
+
+    for (indice = arreglo.length - 1; indice > 0; indice = indice - 1) {
+        aleatorio = Math.floor(Math.random() * (indice + 1));
+        temporal = arreglo[indice];
+        arreglo[indice] = arreglo[aleatorio];
+        arreglo[aleatorio] = temporal;
+    }
+
+    return arreglo;
+}
+
+function crearCartas(nivel) {
+    var totalPares;
+    var cartas;
+    var indice;
+    var simbolo;
+
+    totalPares = PARES_POR_NIVEL[nivel];
+    cartas = [];
+
+    for (indice = 0; indice <= totalPares; indice = indice + 1) {
+        simbolo = SIMBOLOS[indice];
+        cartas.push({ archivo: simbolo.archivo, nombre: simbolo.nombre, parId: indice, emparejada: false });
+        cartas.push({ archivo: simbolo.archivo, nombre: simbolo.nombre, parId: indice, emparejada: false });
+    }
+
+    return mezclarArreglo(cartas);
+}
